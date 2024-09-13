41ec6d8d73f0b54f166eef3bf5e0998872ba692a
Felix Schneider <fs@felix-schneider.org>
2024-09-13 14:25:38 +0200

    Refactor crawler to use a class-based approach
    Refactored the crawler script into a `Crawler` class to improve code organization and maintainability. Integrated logging for better debugging and separated concerns by implementing specific methods for fetching conferences, talks, and handling metadata and audio downloads. This change enhances readability and provides a clear structure for extending functionality in the future.

--- source/crawler.py
+++ source/crawler.py
@@ -1,84 +1,110 @@
 import json
-import os
 from html import unescape
 
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet
 
-BASE_URL = "https://media.ccc.de"
-CONFERENCES_URL = f"{BASE_URL}/b/conferences/gpn"
+from source.logger import LoggerMixin
 
-conferences_page = requests.get(CONFERENCES_URL).content
 
-conferences_soup = BeautifulSoup(conferences_page, "html.parser")
-conferences = conferences_soup.find_all("a", class_="thumbnail conference")
-conferences_links = []
-gpns = []
-for conference in conferences:
-    conference_link = conference["href"]
-    conferences_links.append(conference_link)
-    # Get conference years
-    gpn = conference_link.split("/")[-1]
-    gpns.append(gpn)
+class Crawler(LoggerMixin):
+    def __init__(self):
+        super().__init__()
+        self.BASE_URL = "https://media.ccc.de"
+        self.CONFERENCES_URL = f"{self.BASE_URL}/b/conferences/gpn"
+        self.conferences_links = []
+        self.gpns = []
+        self.talks = {}
 
-for gpn in gpns:
-    path = f"../data/audio/input/{gpn}"
-    if not os.path.exists(path):
-        os.makedirs(path)
+    def run(self) -> None:
+        self.get_conferences()
+        self.get_talks()
 
-talks = {}
-for index, conference_link in enumerate(conferences_links):
-    conference_site = requests.get(BASE_URL + conference_link).content
-    conference_soup = BeautifulSoup(conference_site, "html.parser")
-    talk_elements = conference_soup.find_all("h3")
-    for talk in talk_elements:
-        link = talk.find("a")
-        title = link.text.replace("\n", "")
+    def get_conferences(self) -> None:
+        conferences_page = requests.get(self.CONFERENCES_URL).content
 
-        talk_site = requests.get(BASE_URL + link["href"]).content
-        talk_soup = BeautifulSoup(talk_site, "html.parser")
-        speaker_paragraphs = talk_soup.find("p", class_="persons").find_all("a")
-        speakers = []
-        for speaker in speaker_paragraphs:
-            speakers.append(speaker.text.replace("\n", ""))
+        conferences_soup = BeautifulSoup(conferences_page, "html.parser")
+        conferences = conferences_soup.find_all("a", class_="thumbnail conference")
+        for conference in conferences:
+            link = conference["href"]
+            self.conferences_links.append(link)
+            # Get conference years
+            gpn = link.split("/")[-1]
+            self.gpns.append(gpn)
 
-        metadata_list = talk_soup.find("ul", class_="metadata")
-        metadata = metadata_list.find_all("li")
-        duration = metadata[0].text.replace("\n", "")
-        date = metadata[1].text.replace("\n", "")
+    def get_talks(self) -> None:
+        for index, conference_link in enumerate(self.conferences_links):
+            conference_site = requests.get(self.BASE_URL + conference_link).content
+            conference_soup = BeautifulSoup(conference_site, "html.parser")
+            talk_elements = conference_soup.find_all("h3")
+            self.create_metadata_for_talks(talk_elements, index)
 
-        description_paragraph = talk_soup.find("p", class_="description")
-        description = (
-            description_paragraph.text.replace("\n", "")
-            if description_paragraph
-            else ""
-        )
-        description = unescape(description)
+    def create_metadata_for_talks(
+        self, talks: list[ResultSet], conference_index: int
+    ) -> None:
+        for talk in talks:
+            link = talk.find("a")
+            title = link.text.replace("\n", "")
 
-        metadata = {
-            "title": title,
-            "gpn": gpns[index],
-            "speakers": speakers,
-            "duration": duration,
-            "date": date,
-            "description": description,
-        }
+            self.log.debug(f"Creating metadata file for {title}")
 
+            talk_site = requests.get(self.BASE_URL + link["href"]).content
+            self.talk_soup = BeautifulSoup(talk_site, "html.parser")
+            speaker_paragraphs = self.talk_soup.find("p", class_="persons").find_all(
+                "a"
+            )
+            speakers = []
+            for speaker in speaker_paragraphs:
+                speakers.append(speaker.text.replace("\n", ""))
+
+            metadata_list = self.talk_soup.find("ul", class_="metadata")
+            metadata = metadata_list.find_all("li")
+            duration = metadata[0].text.replace("\n", "")
+            date = metadata[1].text.replace("\n", "")
+
+            description_paragraph = self.talk_soup.find("p", class_="description")
+            description = (
+                description_paragraph.text.replace("\n", "")
+                if description_paragraph
+                else ""
+            )
+            description = unescape(description)
+
+            metadata = {
+                "title": title,
+                "gpn": self.gpns[conference_index],
+                "speakers": speakers,
+                "duration": duration,
+                "date": date,
+                "description": description,
+            }
+
+            self.write_metadata_of_talk(metadata)
+            self.download_audio_of_talk(metadata)
+
+    @staticmethod
+    def write_metadata_of_talk(metadata: dict) -> None:
         with open(
-            f"../data/audio/input/{gpns[index]}/{title.replace('/', ' ')}_metadata.json",
+            f"../data/metadata/{metadata.title.replace('/', ' ')}.json",
             mode="w",
             encoding="utf-8",
         ) as file:
             file.write(json.dumps(metadata, indent=4, ensure_ascii=False))
-            file.close()
 
+    def download_audio_of_talk(self, metadata: dict) -> None:
+        self.log.debug(f"Downloading audio for {metadata.title}")
         # Download and write mp3
-        download_tag = talk_soup.find("div", class_="row audio").find(
+        download_tag = self.talk_soup.find("div", class_="row audio").find(
             "div", string="Download mp3"
         )
         download_link = download_tag.parent["href"]
         response = requests.get(download_link, stream=True)
-        with open(f"../data/audio/input/{gpns[index]}/{title}.mp3", mode="wb") as file:
+        with open(
+            f"../data/audio/{metadata.title.replace('/', ' ')}.mp3", mode="wb"
+        ) as file:
             for chunk in response.iter_content(chunk_size=10 * 1024):
                 file.write(chunk)
-            file.close()
+
+
+crawler = Crawler()
+crawler.run()
