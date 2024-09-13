79ea8e28f07a238b5a58d83dc35686b0d3d259af
Felix Schneider <fs@felix-schneider.org>
2024-09-13 13:39:01 +0200

    Refactor BeautifulSoup assignments for clarity
    Separated requests.get().content calls from BeautifulSoup instantiation for improved readability. This change enhances the clarity of the code by breaking down complex expressions into more manageable and understandable steps.

--- source/crawler.py
+++ source/crawler.py
@@ -1,5 +1,6 @@
 import json
 import os
+from html import unescape
 
 import requests
 from bs4 import BeautifulSoup
@@ -7,7 +8,9 @@ from bs4 import BeautifulSoup
 BASE_URL = "https://media.ccc.de"
 CONFERENCES_URL = f"{BASE_URL}/b/conferences/gpn"
 
-conferences_soup = BeautifulSoup(requests.get(CONFERENCES_URL).content, "html.parser")
+conferences_page = requests.get(CONFERENCES_URL).content
+
+conferences_soup = BeautifulSoup(conferences_page, "html.parser")
 conferences = conferences_soup.find_all("a", class_="thumbnail conference")
 conferences_links = []
 gpns = []
@@ -25,17 +28,15 @@ for gpn in gpns:
 
 talks = {}
 for index, conference_link in enumerate(conferences_links):
-    conference_soup = BeautifulSoup(
-        requests.get(BASE_URL + conference_link).content, "html.parser"
-    )
+    conference_site = requests.get(BASE_URL + conference_link).content
+    conference_soup = BeautifulSoup(conference_site, "html.parser")
     talk_elements = conference_soup.find_all("h3")
     for talk in talk_elements:
         link = talk.find("a")
         title = link.text.replace("\n", "")
 
-        talk_soup = BeautifulSoup(
-            requests.get(BASE_URL + link["href"]).content, "html.parser"
-        )
+        talk_site = requests.get(BASE_URL + link["href"]).content
+        talk_soup = BeautifulSoup(talk_site, "html.parser")
         speaker_paragraphs = talk_soup.find("p", class_="persons").find_all("a")
         speakers = []
         for speaker in speaker_paragraphs:
@@ -46,7 +47,13 @@ for index, conference_link in enumerate(conferences_links):
         duration = metadata[0].text.replace("\n", "")
         date = metadata[1].text.replace("\n", "")
 
-        description = talk_soup.find("p", class_="description").text.replace("\n", "")
+        description_paragraph = talk_soup.find("p", class_="description")
+        description = (
+            description_paragraph.text.replace("\n", "")
+            if description_paragraph
+            else ""
+        )
+        description = unescape(description)
 
         metadata = {
             "title": title,
@@ -58,9 +65,12 @@ for index, conference_link in enumerate(conferences_links):
         }
 
         with open(
-            f"../data/audio/input/{gpns[index]}/{title}_metadata.json", mode="w"
+            f"../data/audio/input/{gpns[index]}/{title.replace('/', ' ')}_metadata.json",
+            mode="w",
+            encoding="utf-8",
         ) as file:
-            file.write(json.dumps(metadata, indent=4))
+            file.write(json.dumps(metadata, indent=4, ensure_ascii=False))
+            file.close()
 
         # Download and write mp3
         download_tag = talk_soup.find("div", class_="row audio").find(
@@ -71,3 +81,4 @@ for index, conference_link in enumerate(conferences_links):
         with open(f"../data/audio/input/{gpns[index]}/{title}.mp3", mode="wb") as file:
             for chunk in response.iter_content(chunk_size=10 * 1024):
                 file.write(chunk)
+            file.close()
