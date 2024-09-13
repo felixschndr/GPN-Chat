7df238f9285c21b1ce5065772e7c832373739ee1
Felix Schneider <fs@felix-schneider.org>
2024-09-13 10:47:07 +0200

    Add docstrings to `Transcriber` class and its methods

--- source/transcriber.py
+++ source/transcriber.py
@@ -8,6 +8,32 @@ from dotenv import load_dotenv
 
 
 class Transcriber:
+    """
+    This module provides a `Transcriber` class that allows you to transcribe audio files using a specified model.
+
+    Usage:
+    ```python
+    transcriber = Transcriber()
+    transcriber.start()
+    ```
+
+    Class:
+        Transcriber
+
+    Methods:
+        - __init__(self): Initializes the Transcriber object.
+        - transcribe_file(self, index: int, file: str) -> None: Transcribes a single audio file.
+        - start(self) -> None: Starts the transcription process for all audio files.
+
+    Attributes:
+        - data_directory: The directory where the data is stored.
+        - transcriber_model_name: The name of the transcriber model.
+        - data_output_directory: The output directory where the transcriptions will be stored.
+        - all_audio_files: A list of all the audio files that need to be transcribed.
+        - number_of_audio_files: The total number of audio files that need to be transcribed.
+    ```
+    """
+
     def __init__(self):
         load_dotenv()
 
@@ -29,6 +55,13 @@ class Transcriber:
         self.number_of_audio_files = len(self.all_audio_files)
 
     def transcribe_file(self, index: int, file: str) -> None:
+        """
+        Transcribes a audio file and saves the transcription in a text file.
+
+        :param index: The index of the audio file in the list of files to transcribe.
+        :param file: The path of the audio file to transcribe.
+        :return: None
+        """
         file_name = os.path.basename(file)
         output_file_name = file_name.replace(".mp3", ".txt")
         output_file_path = os.path.join(self.data_output_directory, output_file_name)
@@ -42,6 +75,11 @@ class Transcriber:
         print("\tFinished transcribing")
 
     def start(self) -> None:
+        """
+        Starts the transcription process of audio files using multiple threads.
+
+        :return: None
+        """
         print("Transcribing audio files...")
         threads = []
         for index, file in enumerate(self.all_audio_files, start=1):
