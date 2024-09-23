e167de13b36d7e212e3d5e6ddd73eef531ca2a90
Felix Schneider <fs@felix-schneider.org>
2024-09-23 09:47:22 +0200

    Revert "Refactor state initialization in `main` method"
    This reverts commit f424739c

--- chatui.py
+++ chatui.py
@@ -2,52 +2,61 @@ import streamlit as st
 
 from source.chatbot import Chatbot
 
-TITLE = "GPN-Chat"
-
 RENDERED_MESSAGES = "rendered_messages"
 CHAT_HISTORY = "chat_history"
 GPN_CHAT_PIPELINE = "gpn_chat_pipeline"
 
 
 def main() -> None:
-    st.set_page_config(page_title=TITLE)
-    st.title(TITLE)
-
-    st.session_state[RENDERED_MESSAGES] = []
-    st.session_state[CHAT_HISTORY] = []
-    st.session_state[GPN_CHAT_PIPELINE] = Chatbot()
-
+    title = "GPN Chat"
+    st.set_page_config(page_title=title)
+    st.title(title)
+    configuration = configure_state()
+    initialize_session_state(configuration)
     render_history()
     run_ui()
 
 
-def render_history() -> None:
-    """
-    Render the chat history stored in the session state. Each message is displayed based on its role and content.
+def configure_state() -> dict:
+    return {
+        RENDERED_MESSAGES: [],
+        CHAT_HISTORY: [],
+        GPN_CHAT_PIPELINE: Chatbot(),
+    }
 
-    :return: None
+
+def initialize_session_state(config: dict) -> None:
     """
+        Initialize Streamlit session state variables using the provided configuration.
+
+    Args:
+        config (dict): Configuration dictionary.
+    """
+    for key, value in config.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
+
+
+def render_history() -> None:
     for message in st.session_state[RENDERED_MESSAGES]:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
 
 def run_ui() -> None:
-    """
-    Accepts user input, processes it, and updates the chat message containers with the user's message and the assistant's response. The function maintains chat history in the session state.
-
-    :return: None
-    """
+    # Accept user input
     if prompt := st.chat_input("Womit kann ich helfen?"):
+        # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
-
+        # Add user message to chat history
         st.session_state[RENDERED_MESSAGES].append({"role": "user", "content": prompt})
 
+        # Display assistant response in chat message container
         with st.chat_message("assistant"):
             with st.spinner("Generiere Antwort ..."):
                 response = st.session_state[GPN_CHAT_PIPELINE].run(prompt)
-
+        # Add assistant response to chat history
         st.session_state[RENDERED_MESSAGES].append(
             {"role": "assistant", "content": response}
         )
